d2d77564ed9bfcf7f464053acec2a5ee0814e016
kenfuciousd <35818192+kenfuciousd@users.noreply.github.com>
2023-11-20 19:16:39 -0800

    upd2

--- server/js/script.js
+++ server/js/script.js
@@ -4,14 +4,34 @@ console.log("script.js loaded");
 import * as THREE from 'three';
 
 document.addEventListener("DOMContentLoaded", function () {
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-    document.getElementById("game-container").appendChild(canvas);
+    // Set up scene
+    const scene = new THREE.Scene();
 
-    canvas.width = 800;
-    canvas.height = 600;
+    // Set up camera
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
 
-    // Example: Draw a rectangle
-    ctx.fillStyle = "#00F";
-    ctx.fillRect(50, 50, 100, 100);
+    // Set up renderer
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById("game-container").appendChild(renderer.domElement);
+
+    // Example: Add a cube to the scene
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    // Set up animation
+    const animate = function () {
+        requestAnimationFrame(animate);
+
+        // Example: Rotate the cube
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+    };
+
+    animate();
 });
